85fc6b774e2f0b0d1b165d4e906e4a57562e23bd
MohammedMoshood <moshood.mm8@gmail.com>
2022-10-25 01:00:40 +0100

    fix

--- src/components/FrontPage/HeroSection.jsx
+++ src/components/FrontPage/HeroSection.jsx
@@ -23,7 +23,7 @@ const HeroSection = () => {
     <Hero>
       <HeroContainer>
         <HeroLeft>
-          <HeroH>Social media and e-Commerce meets mobile payment.</HeroH>
+          <HeroH>Social media and <br/>e-commerce meets mobile payment.</HeroH>
           <HeroP>
           Slydo is a Social Media, E-commerce, and Mobile Payment app that connects people to their friends and the world.
           </HeroP>
