081620ce20dc17496c94584d5d651d7fc231082b
MohammedMoshood <moshood.mm8@gmail.com>
2022-11-06 22:41:31 +0100

    coming soon page fix

--- src/pages/UnderConstruction.jsx
+++ src/pages/UnderConstruction.jsx
@@ -7,41 +7,106 @@ import ErrorImage from "../images/UnderConstruct.svg";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-const TextDiv = styled.div`
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  text-align: center;
-`;
-const Text = styled.h3`
-  @media screen and (max-width: 768px) {
-    font-size: 15px;
+const RightDiv = styled.div`
+  display: flex;
+  @media screen and (max-width: 1020px) {
+    width: 100%;
+    justify-content: center;
   }
 `;
-const Button = styled.button`
-  background: #3f61db;
-  padding: 5px;
-  width: 150px;
-  outline: none;
-  border: none;
-  color: white;
-  border-radius: 5px;
-  margin-top: 20px;
-  transition: 0.2s all;
-  @media screen and (max-width: 768px) {
-    width: 100px;
-    font-size: 12px;
+const LeftDiv = styled.div`
+  width: 50%;
+
+  margin-bottom: 100px;
+  @media screen and (max-width: 1020px) {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    gap: 40px;
   }
-  &:hover {
-    background: #6a85e6;
+`;
+const FeatureDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  @media screen and (max-width: 1020px) {
+    justify-content: center;
   }
 `;
 
-const ButtonLink = styled.a`
-  text-decoration: none;
-  color: white;
-  &:hover {
-    color: white;
+const Text = styled.h2`
+  margin-top: 40px;
+  font-size: 50px;
+  @media screen and (max-width: 1020px) {
+    font-size: 30px;
+  }
+`;
+
+const Image = styled.img`
+  width: 90%;
+  @media screen and (max-width: 1020px) {
+    width: 80%;
+  }
+`;
+
+const TopDiv = styled.div`
+  gap: 20px;
+  display: flex;
+`;
+const BottomDiv = styled.div`
+  gap: 20px;
+  display: flex;
+  @media screen and (max-width: 1020px) {
+    width: 70%;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  @media screen and (max-width: 768px) {
+    width: 80%;
+  }
+  @media screen and (max-width: 480px) {
+    width: 100%;
+  }
+`;
+const SingleFeature = styled.div`
+  padding: 15px;
+  border-radius: 10px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: default;
+  @media screen and (max-width: 480px) {
+    padding:10px ;
+    height: 30px;
+  }
+`;
+const FeatureText = styled.p`
+  font-family: "Inter";
+  font-weight: 500;
+  @media screen and (max-width: 768px) {
+    font-size: 12px;
+  }
+  @media screen and (max-width: 480px) {
+    font-size: 10px;
+  }
+  
+`;
+
+const ConstructionDiv = styled.div`
+  width: 84%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: center;
+  margin: 0 auto;
+  padding-top: 50px;
+  @media screen and (max-width: 1020px) {
+    flex-direction: column;
+    align-items: center;
+
   }
 `;
 const UnderConstruction = () => {
@@ -54,29 +119,72 @@ const UnderConstruction = () => {
           width: "100%",
           maxWidth: "1680px",
           background: "white",
-          padding: "30px",
           display: "flex",
-          justifyContent: "center",
+          height: "fit-content",
+          flexDirection: "column",
+          justifyContent: "flex-end",
         }}
       >
-        <div
-          style={{
-            width: "84%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            margin: "0 auto",
-          }}
-        >
-          <img src={ErrorImage} alt="construction"></img>
-          <TextDiv>
-            <Text>We are working an creating amazing experience just for you. Please check back later.</Text>
-            <Button>
-              <ButtonLink href="/" rel="noreferrer"> Back to Home</ButtonLink>
-            </Button>
-          </TextDiv>
-        </div>
+        <ConstructionDiv style={{}}>
+          <LeftDiv>
+            <FeatureDiv>
+              <TopDiv>
+                <SingleFeature style={{ backgroundColor: " rgba(255, 200, 188, 0.5)", color: "rgba(220, 65, 31, 1)" }}>
+                  <FeatureText>
+                    <i>socials</i>
+                  </FeatureText>
+                </SingleFeature>
+                <SingleFeature style={{ backgroundColor: "rgba(184, 198, 255, 0.5)", color: "rgba(58, 96, 248, 1)" }}>
+                  <FeatureText>
+                    <i>payment</i>
+                  </FeatureText>
+                </SingleFeature>
+                <SingleFeature style={{ backgroundColor: "rgba(232, 182, 255, 0.5)", color: "rgba(167, 23, 235, 1)" }}>
+                  <FeatureText>
+                    <i>business</i>
+                  </FeatureText>
+                </SingleFeature>
+                <SingleFeature style={{ backgroundColor: "rgba(243, 175, 208, 0.5)", color: "rgba(223, 48, 132, 1)" }}>
+                  <FeatureText>
+                    <i>developers</i>
+                  </FeatureText>
+                </SingleFeature>
+              </TopDiv>
+              <BottomDiv>
+                <SingleFeature style={{ backgroundColor: "rgba(180, 255, 216, 0.5)", color: "rgba(16, 168, 89, 1)" }}>
+                  <FeatureText>
+                    <i>chat</i>
+                  </FeatureText>
+                </SingleFeature>
+                <SingleFeature style={{ backgroundColor: "rgba(243, 175, 208, 0.5)", color: "rgba(223, 48, 132, 1)" }}>
+                  <FeatureText>
+                    <i>moments</i>
+                  </FeatureText>
+                </SingleFeature>
+                <SingleFeature style={{ backgroundColor: "rgba(180, 255, 216, 0.5)", color: "rgba(16, 168, 89, 1)" }}>
+                  <FeatureText>
+                    <i>commerce</i>
+                  </FeatureText>
+                </SingleFeature>
+                <SingleFeature style={{ backgroundColor: "rgba(198, 210, 255, 0.5)", color: "rgba(63, 97, 219, 1)" }}>
+                  <FeatureText>money</FeatureText>
+                </SingleFeature>
+                <SingleFeature style={{ backgroundColor: "rgba(223, 223, 226, 0.5)", color: "rgba(21, 21, 21, 1)" }}>
+                  <FeatureText>mobile</FeatureText>
+                </SingleFeature>
+                <SingleFeature style={{ backgroundColor: "rgba(255, 224, 180, 0.5)", color: "rgba(206, 123, 0, 1)" }}>
+                  <FeatureText>digital</FeatureText>
+                </SingleFeature>
+              </BottomDiv>
+            </FeatureDiv>
+            <Text>
+              We are creating <br /> an amazing experience <br /> Please check back
+            </Text>
+          </LeftDiv>
+          <RightDiv>
+            <Image src={ErrorImage} alt="construction"></Image>
+          </RightDiv>
+        </ConstructionDiv>
       </div>
       <Footer />
     </div>
